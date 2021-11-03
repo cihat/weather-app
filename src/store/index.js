8e3db8600422b4965d3c100fc2b2996b5fe6f6a6
cihat <cihat02020202@gmail.com>
2021-11-03 16:32:44 +0300

    Add fetchWeather action

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,25 @@
 import { createStore } from "vuex";
+import axios from 'axios';
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    weather: {},
+  },
+  mutations: {
+    setWeather(state, weather) {
+      state.weather = weather;
+    }
+  },
+  actions: {
+    fetchWeather({ commit }, city) {
+      return axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.VUE_APP_API_KEY}`)
+        .then(response => {
+          commit('setWeather', response.data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  },
   modules: {},
 });
