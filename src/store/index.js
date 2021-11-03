66d654659650f7534ffd7f83e130afa7786879e0
cihat <cihat02020202@gmail.com>
2021-11-03 19:39:24 +0300

    Configure state managment that vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,43 @@
 import { createStore } from "vuex";
 import axios from 'axios';
 
+const API_KEY = process.env.VUE_APP_API_KEY
+
 export default createStore({
   state: {
     weather: {},
+    location: {}
   },
   mutations: {
     setWeather(state, weather) {
-      state.weather = weather;
+      state.weather = weather
+    },
+    setLocation(state, location) {
+      state.location = location
     }
   },
   actions: {
-    fetchWeather({ commit }, city) {
-      return axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.VUE_APP_API_KEY}`)
+    async fetchWeather({ commit }, city) {
+      return await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
         .then(response => {
           commit('setWeather', response.data);
         })
         .catch(error => {
           console.log(error);
         });
+    },
+    async fetchLocation({ commit }, location) {
+      return await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${location.lat}&lon=${location.lon}&appid=${API_KEY}`)
+        .then(response => {
+          commit('setLocation', response.data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    async fetchWeatherAndLocation({ dispatch }, location) {
+      await dispatch('fetchLocation', location);
+      await dispatch('fetchWeather', location.city);
     }
   },
   modules: {},
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,59 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Weather App</h1>
+    <input
+      type="text"
+      name="input-city"
+      id="weather"
+      v-model="city"
+      v-on:keyup.enter="getWeather"
+    />
+    <p>{{ city }}</p>
+    <div class="result">
+      <p v-if="errMessage">{{ errMessage }}</p>
+      <p v-else-if="isLoading">Please wait..⌛️.</p>
+      <p v-else>{{ JSON.stringify(weather) }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import { mapActions, mapState } from "vuex"
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      city: "",
+      localWeather: null,
+      errMessage: "",
+      isLoading: true,
+    }
   },
-};
+  computed: {
+    ...mapState(["weather"]),
+  },
+  created() {
+    this.weather = {
+      hello: "helloo",
+    }
+  },
+  created() {
+    this.getWeather("Adıyaman")
+  },
+  methods: {
+    ...mapActions(["fetchWeather"]),
+    async getWeather(city) {
+      try {
+        await this.fetchWeather(this.city || city)
+      } catch (error) {
+        this.errMessage = error.message
+      } finally {
+        this.isLoading = false
+      }
+    },
+  },
+}
 </script>
+
+<style lang="scss"></style>
