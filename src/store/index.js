c4cdb11564d95ab8b9872bf239c7074e0fa72794
cihat <cihat02020202@gmail.com>
2021-11-03 22:24:39 +0300

    Refactor store that vuex

--- src/store/index.js
+++ src/store/index.js
@@ -6,39 +6,26 @@ const API_KEY = process.env.VUE_APP_API_KEY
 export default createStore({
   state: {
     weather: {},
-    location: {}
+  },
+  getters: {
+    getWeather: state => (state.weather),
   },
   mutations: {
     setWeather(state, weather) {
       state.weather = weather
     },
-    setLocation(state, location) {
-      state.location = location
-    }
   },
   actions: {
     async fetchWeather({ commit }, city) {
       return await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
+        .then(response => JSON.parse(JSON.stringify(response.data)),)
         .then(response => {
-          commit('setWeather', response.data);
+          commit('setWeather', response);
         })
         .catch(error => {
           console.log(error);
         });
     },
-    async fetchLocation({ commit }, location) {
-      return await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${location.lat}&lon=${location.lon}&appid=${API_KEY}`)
-        .then(response => {
-          commit('setLocation', response.data);
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    },
-    async fetchWeatherAndLocation({ dispatch }, location) {
-      await dispatch('fetchLocation', location);
-      await dispatch('fetchWeather', location.city);
-    }
   },
   modules: {},
 });
