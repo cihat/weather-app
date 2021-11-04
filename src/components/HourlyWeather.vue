9c7b882d9ef99fe5d12757993c5a030f78f1f2cb
cihat <cihat02020202@gmail.com>
2021-11-04 11:15:30 +0300

    Add HourlyWeather comp.

---
+++ src/components/HourlyWeather.vue
@@ -0,0 +1,36 @@
+<script>
+import { mapState, mapActions, mapGetters } from "vuex"
+
+export default {
+  name: "HourlyWeather",
+  computed: {
+    ...mapGetters({
+      weather: "getWeather",
+    }),
+  },
+}
+</script>
+
+<template>
+  <div class="d-flex flex-row overflow-auto border-top border-bottom pb-2 pt-2">
+    <div
+      className="d-flex flex-column col-2 align-items-center px-4 "
+      key="{hour.dt}"
+    >
+      <p className="h-25">{time}</p>
+      <p className="text-primary">
+        {hour.pop > 0 ? Math.round(hour.pop * 100) + '%' : ''}
+      </p>
+      <figure>
+        <img
+          className="img-fluid weather-img"
+          :src="`https://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png`"
+        />
+      </figure>
+
+      <p className="">{{ weather.main.temp }}</p>
+    </div>
+  </div>
+</template>
+
+<style></style>
